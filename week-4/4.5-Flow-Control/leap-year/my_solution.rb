1b4a037beaea22001890e754f4aedcf218aeba75
Pratik Shah <pratik.t.shah@gmail.com>
2015-10-21 15:40:59 -0500

    Leap Year Program

---
+++ week-4/4.5-Flow-Control/leap-year/my_solution.rb
@@ -0,0 +1,21 @@
+# Leap Years
+
+# I worked on this challenge [by myself, with: Sabrina].
+
+
+# Your Solution Below
+def leap_year?(year)
+  if (year % 4 == 0)
+    if(year % 100 == 0)
+      if(year % 400 == 0)
+        p true
+      else
+        p false
+      end
+    else
+      p true
+    end
+  else
+    p false
+  end
+end
