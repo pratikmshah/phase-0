60d8ea10c9a13876447a262f3b0ba36139e2c076
Pratik Shah <pratik.t.shah@gmail.com>
2015-11-03 15:52:07 -0600

    Guess Game Pt2

--- week-6/guessing-game/my_solution.rb
+++ week-6/guessing-game/my_solution.rb
@@ -14,7 +14,7 @@
 # step3: create METHOD solved? to return true or false based on the last guess
 
 
-# Initial Solution
+Initial Solution
 
 class GuessingGame
   def initialize(answer)
@@ -46,29 +46,60 @@ class GuessingGame
   end
 end
 
+# ---Refactored Solution---
+class GuessingGame
+  def initialize(answer)
+    @answer = answer
+  end
+
+  def guess(guess)
+    @guess = guess
+    if(guess > @answer)
+      p :high
+    elsif(guess < @answer)
+      p :low
+    else
+      p :correct
+    end
+  end
+
+  def solved?
+    @solve = true if(@answer == @guess)
+  end
+end
+
 # Tests
 game = GuessingGame.new(15)
 
 while !game.solved?
-  puts "Enter a number between "
-
+  print "Enter a number between 1-15: "
+  answer = gets.chomp.to_i
+  game.guess(answer)
+  game.solved?
 end
-game = GuessingGame.new(10)
-
-game.solved?   # => false
-
-game.guess(5)  # => :low
-game.guess(20) # => :high
-game.solved?   # => false
-
-game.guess(10) # => :correct
-game.solved?   # => true
-
-# Refactored Solution
-
-
-
-
-
 
 # Reflection
+=begin
+
+How do instance variables and methods represent the characteristics and behaviors (actions) of a real-world object?
+- instant variables represent and methods represent the behaviors of a real world object becuase you can think of
+  variables as storing the attriubtes of the object while the methods being the actions of and object. For instance
+  take human body we have eyes, ears, nose, mouth etc... examples of variables would be eyes color and method would
+  be to see, to eat, smell, hear etc...
+
+When should you use instance variables? What do they do for you?
+- Instance variables are good for tracking data within a class that objects that are apart of that class can
+  access without first having to define them. The instance variables do not lose scope outside of the class as long
+  as you have an object that can call on them.
+
+Explain how to use flow control. Did you have any trouble using it in this challenge? If so, what did you struggle with?
+- flow control is implemented by using if else statements and loops to guide the ruby interpreter on what to do when
+  in different scenarios. I only had one issue and it was on how to transfer the user input to the solved? method but I
+  found out you could create an instance variable inside of a method and it is still accessable outside.
+
+Why do you think this code requires you to return symbols? What are the benefits of using symbols?
+- Symbols allow you to represet names and strings all in one which makes it more memory efficient.
+  For example when you create a hash ruby has to create a new string object in a different memory location vs
+  when you use a symbol which gets created once stored in memory and reusable multiple times over serveral instances.
+
+=end
